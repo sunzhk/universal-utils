1d0b5395ad3b047e708f4e070bb156d03e877812
sunzhk <312797831@qq.com>
2016-10-17 19:34:06 +0800

    优化：getDirectorySize的回调会在主线程进行

--- universal-utils/src/main/java/com/sunzhk/tools/utils/file/FileUtils.java
+++ universal-utils/src/main/java/com/sunzhk/tools/utils/file/FileUtils.java
@@ -4,6 +4,10 @@ import com.sunzhk.tools.utils.MultiThreadTraversal;
 
 import java.io.File;
 
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+
 /**
  * 文件工具类
  */
@@ -11,16 +15,34 @@ public class FileUtils {
 
 	/**
 	 * 计算文件夹及其子文件、子文件夹的大小
-	 * @param directory
-	 * @param callBack 会在子线程回调
+	 * @param directory 需要计算大小的文件夹。若没有子文件或不是文件夹则返回0
+	 * @param callBack 会在主线程里回调
 	 */
 	public static void getDirectorySize(File directory, final OnAnalyzeDirectorySize callBack){
 		if(callBack == null){
 			return;
 		}
+		if(directory.isFile()){
+			Observable.just(directory.length())
+					.observeOn(AndroidSchedulers.mainThread())
+					.subscribe(new Action1<Long>() {
+						@Override
+						public void call(Long aLong) {
+							callBack.onResult(aLong);
+						}
+					});
+			return;
+		}
 		if(!directory.isDirectory() || directory.listFiles() == null || directory.listFiles().length == 0){
-				callBack.onResult(0L);
-				return;
+			Observable.just(0L)
+					.observeOn(AndroidSchedulers.mainThread())
+					.subscribe(new Action1<Long>() {
+						@Override
+						public void call(Long aLong) {
+							callBack.onResult(aLong);
+						}
+					});
+			return;
 		}
 		new MultiThreadTraversal().exce(directory, new MultiThreadTraversal.AnalyzerFilter<File>() {
 
@@ -46,7 +68,14 @@ public class FileUtils {
 
 			@Override
 			public void onFinished() {
-				callBack.onResult(size);
+				Observable.just(size)
+						.observeOn(AndroidSchedulers.mainThread())
+						.subscribe(new Action1<Long>() {
+							@Override
+							public void call(Long aLong) {
+								callBack.onResult(aLong);
+							}
+						});
 			}
 
 			private synchronized void addSize(long childSize){

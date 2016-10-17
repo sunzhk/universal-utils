4d87914e4d83e630d4a1f95c974f4189b0857eeb
sunzhk <312797831@qq.com>
2016-10-17 16:05:04 +0800

    更新文件夹大小计算

--- universal-utils/src/main/java/com/sunzhk/tools/utils/FileUtils.java
+++ universal-utils/src/main/java/com/sunzhk/tools/utils/FileUtils.java
@@ -2,18 +2,64 @@ package com.sunzhk.tools.utils;
 
 import java.io.File;
 
+/**
+ * 文件工具类
+ */
 public class FileUtils {
 
-	public long getFileSize(File file){
-		if(file == null || !file.exists()){
-			return -1;
+	/**
+	 * 计算文件夹及其子文件、子文件夹的大小
+	 * @param directory
+	 * @param callBack 会在子线程回调
+	 */
+	public static void getDirectorySize(File directory, final OnAnalyzeDirectorySize callBack){
+		if(callBack == null){
+			return;
 		}
-		if(file.isFile()){
-			return file.length();
-		}else{
-			
+		if(!directory.isDirectory() || directory.listFiles() == null || directory.listFiles().length == 0){
+				callBack.onResult(0L);
+				return;
 		}
-		return 0;
+		new MultiThreadTraversal().exce(directory, new MultiThreadTraversal.AnalyzerFilter<File>() {
+
+			private long size;
+
+			@Override
+			public File[] getChild(File rootNode) {
+				return rootNode.listFiles();
+			}
+
+			@Override
+			public boolean hasChild(File rootNode) {
+				return rootNode.listFiles() != null && rootNode.listFiles().length >0;
+			}
+
+			@Override
+			public void process(File node) {
+				if(node.length() == 0){
+					return;
+				}
+				addSize(node.length());
+			}
+
+			@Override
+			public void onFinished() {
+				callBack.onResult(size);
+			}
+
+			private synchronized void addSize(long childSize){
+				size += childSize;
+			}
+
+		});
+	}
+
+	public interface OnAnalyzeDirectorySize{
+		/**
+		 *
+		 * @param size in bytes
+		 */
+		void onResult(long size);
 	}
 	
 }

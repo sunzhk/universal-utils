8fdf36d1a90076e0e1135a242c5b4d18418620d6
sunzhk <312797831@qq.com>
2016-11-01 20:19:24 +0800

    更新：添加字符串工具类

---
+++ universal-utils/src/main/java/com/sunzhk/tools/utils/PermissionUtils.java
@@ -0,0 +1,8 @@
+package com.sunzhk.tools.utils;
+
+/**
+ * Created by sunzhk on 2016/10/18.
+ */
+
+public class PermissionUtils {
+}
---
+++ universal-utils/src/main/java/com/sunzhk/tools/utils/StringUtils.java
@@ -0,0 +1,8 @@
+package com.sunzhk.tools.utils;
+
+/**
+ * Created by sunzhk on 2016/10/30.
+ */
+
+public class StringUtils {
+}

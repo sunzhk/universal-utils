bcac1abd2e7615830f3803b6df425e683195482a
sunzhk <312797831@qq.com>
2017-01-09 13:54:39 +0800

    补充危险权限

--- universal-utils/src/main/java/com/sunzhk/tools/utils/PermissionUtils.java
+++ universal-utils/src/main/java/com/sunzhk/tools/utils/PermissionUtils.java
@@ -1,5 +1,6 @@
 package com.sunzhk.tools.utils;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -24,38 +25,48 @@ public class PermissionUtils {
 	public enum DangerousPermissions {
 
 		//CALENDAR
-		READ_CALENDAR,
-		WRITE_CALENDAR,
+		READ_CALENDAR(Manifest.permission.READ_CALENDAR),
+		WRITE_CALENDAR(Manifest.permission.WRITE_CALENDAR),
 		//CAMERA
-		CAMERA,
+		CAMERA(Manifest.permission.CAMERA),
 		//CONTACTS
-		READ_CONTACTS,
-		WRITE_CONTACTS,
-		GET_ACCOUNTS,
+		READ_CONTACTS(Manifest.permission.READ_CONTACTS),
+		WRITE_CONTACTS(Manifest.permission.WRITE_CONTACTS),
+		GET_ACCOUNTS(Manifest.permission.GET_ACCOUNTS),
 		//LOCATION
-		ACCESS_FINE_LOCATION,
-		ACCESS_COARSE_LOCATION,
+		ACCESS_FINE_LOCATION(Manifest.permission.ACCESS_FINE_LOCATION),
+		ACCESS_COARSE_LOCATION(Manifest.permission.ACCESS_COARSE_LOCATION),
 		//MICROPHONE
-		RECORD_AUDIO,
-		//PHONE,
-		READ_PHONE_STATE,
-		CALL_PHONE,
-		READ_CALL_LOG,
-		WRITE_CALL_LOG,
-		ADD_VOICEMAIL,
-		USE_SIP,
-		PROCESS_OUTGOING_CALLS,
+		RECORD_AUDIO(Manifest.permission.RECORD_AUDIO),
+		//PHONE(Manifest.permission.),
+		READ_PHONE_STATE(Manifest.permission.READ_PHONE_STATE),
+		CALL_PHONE(Manifest.permission.CALL_PHONE),
+		READ_CALL_LOG(Manifest.permission.READ_CALL_LOG),
+		WRITE_CALL_LOG(Manifest.permission.WRITE_CALL_LOG),
+		ADD_VOICEMAIL(Manifest.permission.ADD_VOICEMAIL),
+		USE_SIP(Manifest.permission.USE_SIP),
+		PROCESS_OUTGOING_CALLS(Manifest.permission.PROCESS_OUTGOING_CALLS),
 		//SENSORS
-		BODY_SENSORS,
+		BODY_SENSORS(Manifest.permission.BODY_SENSORS),
 		//SMS
-		SEND_SMS,
-		RECEIVE_SMS,
-		READ_SMS,
-		RECEIVE_WAP_PUSH,
-		RECEIVE_MMS,
+		SEND_SMS(Manifest.permission.SEND_SMS),
+		RECEIVE_SMS(Manifest.permission.RECEIVE_SMS),
+		READ_SMS(Manifest.permission.READ_SMS),
+		RECEIVE_WAP_PUSH(Manifest.permission.RECEIVE_WAP_PUSH),
+		RECEIVE_MMS(Manifest.permission.RECEIVE_MMS),
 		//STORAGE
-		READ_EXTERNAL_STORAGE,
-		WRITE_EXTERNAL_STORAGE,
+		READ_EXTERNAL_STORAGE(Manifest.permission.READ_EXTERNAL_STORAGE),
+		WRITE_EXTERNAL_STORAGE(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+		private String content;
+
+		DangerousPermissions(String content) {
+			this.content = content;
+		}
+
+		public String getContent() {
+			return content;
+		}
 	}
 
 	public static boolean checkPermission(@NonNull Context context, @NonNull String permission) {
@@ -70,7 +81,7 @@ public class PermissionUtils {
 	 * @param requestCode 请求码
 	 */
 	public static void getPermission(@NonNull Activity context, @NonNull DangerousPermissions permission, int requestCode) {
-		getPermission(context, permission.name(), requestCode);
+		getPermission(context, permission.getContent(), requestCode);
 	}
 
 	/**

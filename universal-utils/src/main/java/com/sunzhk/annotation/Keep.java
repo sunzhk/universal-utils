a0909962c65ed8a0e61cb2f94d82d4cf394d8ca0
sunzhk <312797831@qq.com>
2017-03-07 14:44:06 +0800

    增加注解，用于混淆时过滤部分类、方法、参数。

--- universal-utils/src/main/java/com/sunzhk/annotation/Keep.java
+++ universal-utils/src/main/java/com/sunzhk/annotation/Keep.java
@@ -7,6 +7,12 @@ import java.lang.annotation.Target;
 
 /**
  * 在混淆时过滤掉带有该注解的类(不包括类里面的方法、参数)、方法、参数等。
+ * 需要在混淆文件proguard-rules.pro中加入
+ * -keep,allowobfuscation @interface com.sunzhk.annotation.Keep
+ * -keep @com.sunzhk.annotation.Keep class *
+ * -keepclassmembers class * {
+ *     @com.sunzhk.annotation.Keep *;
+ * }
  */
 @Retention(RetentionPolicy.CLASS)
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.FIELD})
--- universal-utils/src/main/java/com/sunzhk/annotation/KeepAll.java
+++ universal-utils/src/main/java/com/sunzhk/annotation/KeepAll.java
@@ -7,6 +7,9 @@ import java.lang.annotation.Target;
 
 /**
  * 在混淆时过滤掉带有该注解的整个类。
+ * 需要在混淆文件proguard-rules.pro中加入
+ * -keep,allowobfuscation @interface com.sunzhk.annotation.KeepAll
+ * -keep @com.sunzhk.annotation.KeepAll class * {*;}
  */
 @Retention(RetentionPolicy.CLASS)
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.FIELD})
